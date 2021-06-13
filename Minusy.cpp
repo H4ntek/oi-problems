6616fe84f4fae96a02ffad73d3f42a3d35a8b9cb
H4ntek <85628860+H4ntek@users.noreply.github.com>
2021-06-13 19:22:21 +0200

    Add files via upload

---
+++ Minusy.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+typedef long long int lli;
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    char last, next;
+    cin >> last;
+    cout << last;
+    for (int i = 0; i < n - 2; i++){
+        cin >> next;
+        if (next != last){
+            if (next == '+'){
+                cout << '(';
+            }
+            else{
+                cout << ')';
+            }
+        }
+        cout << '-';
+        last = next;
+    }
+    if (last == '+'){
+        cout << ')';
+    }
+    cout << "\n";
+}
+
+int main (){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    t = 1;
+    //cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
