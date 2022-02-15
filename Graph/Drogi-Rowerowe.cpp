a1e941f3098189e63e3e58b39519200e7662ccc9
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-02-15 08:23:16 +0100

    Update Drogi-Rowerowe.cpp

--- Graph/Drogi-Rowerowe.cpp
+++ Graph/Drogi-Rowerowe.cpp
@@ -2,91 +2,113 @@
 typedef long long int lli;
 using namespace std;
 
-const int maxn = 5e4 + 3;
+const int MAXN = 5e4 + 3;
 
-vector <int> adj[maxn], adj_t[maxn], adj_cc[maxn];
-vector <int> postorder;
+vector <int> adj[MAXN], adj_t[MAXN], adj_cc[MAXN];
+int comp[MAXN], comp_sz[MAXN], dp[MAXN];
+bool vis_cc[MAXN];
 
-int which_cc[maxn], cc_size[maxn], dp[maxn];
-bool vis_1[maxn], vis_cc[maxn];
+struct SCC{
+    int n, comp_no = 0;
+    vector <int> preorder, component;
+    vector <bool> vis;
 
-void DFS_1(int v){
-    vis_1[v] = true;
-    for (int child : adj[v]){
-        if (!vis_1[child]){
-            DFS_1(child);
-        }
+    void init(int n_){
+        n = n_;
+        preorder.clear();
+        component.clear();
     }
-    postorder.push_back(v);
-}
 
-void DFS_2(int v, int num){
-    which_cc[v] = num;
-    cc_size[num]++;
-    for (int child : adj_t[v]){
-        if (which_cc[child] == 0){
-            DFS_2(child, num);
+    void DFS1(int v){
+        vis[v] = true;
+        for (int child : adj[v]){
+            if (!vis[child]){
+                DFS1(child);
+            }
         }
-        else{
-            if (adj_cc[which_cc[child]].empty() || adj_cc[which_cc[child]].back() != num){
-                if (which_cc[child] != num){
-                    adj_cc[which_cc[child]].push_back(num);
+        preorder.push_back(v);
+    }
+
+    void DFS2(int v){
+        vis[v] = true;
+        comp[v] = comp_no;
+        comp_sz[comp_no]++;
+        component.push_back(v);
+        for (int child : adj_t[v]){
+            if (!vis[child]){
+                DFS2(child);
+            }
+            else{
+                if (adj_cc[comp[child]].empty() || adj_cc[comp[child]].back() != comp_no){
+                    if (comp[child] != comp_no){
+                        adj_cc[comp[child]].push_back(comp_no);
+                    }
                 }
             }
         }
     }
-}
 
-void DFS_cc(int v, int par){
+    vector <vector <int>> run(){
+        vector <vector <int>> components;
+        vis.assign(n + 1, false);
+        for (int i = 1; i <= n; i++){
+            if (!vis[i]){
+                DFS1(i);
+            }
+        }
+        vis.assign(n + 1, false);
+        for (int i = n - 1; i >= 0; i--){
+            if (!vis[preorder[i]]){
+                comp_no++;
+                DFS2(preorder[i]);
+                components.push_back(component);
+                component.clear();
+            }
+        }
+        return components;
+    }
+};
+
+void DFS_dp(int v, int par){
     vis_cc[v] = true;
-    dp[v] = cc_size[v];
+    dp[v] = comp_sz[v];
     for (int child : adj_cc[v]){
         if (child != par){
-            DFS_cc(child, v);
+            DFS_dp(child, v);
             dp[v] += dp[child];
         }
     }
 }
 
 void solve(){
-    int n, m, a, b;
+	int n, m, a, b;
     cin >> n >> m;
     for (int i = 0; i < m; i++){
         cin >> a >> b;
         adj[a].push_back(b);
         adj_t[b].push_back(a);
     }
+    SCC scc;
+    scc.init(n);
+    vector <vector <int>> sccs = scc.run();
     for (int i = 1; i <= n; i++){
-        if (!vis_1[i]){
-            DFS_1(i);
-        }
-    }
-    reverse(postorder.begin(), postorder.end());
-    int cc_num = 0;
-    for (int v : postorder){
-        if (which_cc[v] == 0){
-            cc_num++;
-            DFS_2(v, cc_num);
-        }
-    }
-    for (int i = 1; i <= cc_num; i++){
         if (!vis_cc[i]){
-            DFS_cc(i, -1);
+            DFS_dp(i, -1);
         }
     }
     for (int i = 1; i <= n; i++){
-        cout << dp[which_cc[i]] - 1 << "\n";
+        cout << dp[comp[i]] - 1 << "\n";
     }
 }
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    t = 1;
-    //cin >> t;
-    while (t--){
-        solve();
-    }
-    return 0;
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	int t;
+	t = 1;
+	//cin >> t;
+	while (t--){
+		solve();
+	}
+	return 0;
 }
