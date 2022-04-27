92e372b7a4256ca04ca6b2d6f98bd7eb90f8e664
H4ntek <85628860+H4ntek@users.noreply.github.com>
2022-04-27 18:25:59 +0200

    Update Luk-Triumfalny.cpp

--- Trees/Luk-Triumfalny.cpp
+++ Trees/Luk-Triumfalny.cpp
@@ -1,55 +1,58 @@
-//XX OI
 #include <bits/stdc++.h>
 typedef long long int lli;
 using namespace std;
 
-const int maxn = 3e5 + 3;
+const int MAXN = 3e5 + 7;
 
-vector <int> adj[maxn];
-int dp[maxn];
+int k;
+vector <int> adj[MAXN];
+int dp[MAXN]; // ile łuków trzeba zbudować przed wejściem króla do v
 
-int DFS(int node, int par, int &k){
-	int amount = 0;
-	for (int child : adj[node]){
-		if (child != par){
-			amount += DFS(child, node, k) + 1;
-		}
-	}
-	amount -= k;
-	return dp[node] = max(0, amount);
+void DFS(int v, int par){ // mamy k ekip
+    int trzeba = 0;
+    for (int child : adj[v]){
+        if (child != par){
+            DFS(child, v);
+            trzeba += dp[child] + 1;
+        }
+    }
+    trzeba -= k; // używamy k ekip w tej "rundzie"
+    dp[v] = max(trzeba, 0);
 }
 
 void solve(){
-	int n, a, b;
-	cin >> n;
-	for (int i = 0; i < n - 1; i++){
-		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
-	}
-	int l = 0, r = n, mid;
-	while (l < r){
-		mid = (l + r)/2;
-		dp[1] = DFS(1, -1, mid);
-		if (dp[1] == 0){
-			r = mid;
-		}
-		else{
-			l = mid + 1;
-		}
-		memset(dp, 0, sizeof(dp));
-	}
-	cout << l << "\n";
+    int n, a, b;
+    cin >> n;
+    for (int i = 0; i < n - 1; i++){
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    int l = 0, r = n, mid, ans = n;
+    while (l <= r){
+        mid = (l + r) / 2;
+        k = mid;
+        DFS(1, -1); // czy można z k ekipami
+        if (dp[1] == 0){
+            r = mid - 1;
+            ans = min(ans, mid);
+        }
+        else{
+            l = mid + 1;
+        }
+        memset(dp, 0, sizeof(dp));
+    }
+    cout << ans << "\n";
 }
 
-int main(){
-    ios_base::sync_with_stdio(0);
+int main (){
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
-    //cin >> t;
     t = 1;
+    //cin >> t;
     while (t--){
         solve();
     }
     return 0;
-}
+}   
